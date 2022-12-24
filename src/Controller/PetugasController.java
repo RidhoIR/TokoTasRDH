20da2476fb3d5d5a6e680cb659e002664d0ab150
RidhoIR <ridhoikhsandria348@gmail.com>
2022-12-24 17:16:05 +0700

    modul 11-12

--- src/Controller/PetugasController.java
+++ src/Controller/PetugasController.java
@@ -7,41 +7,7 @@ import java.util.Scanner;
 
 public class PetugasController {
 
-    PetugasModel ptgs = new PetugasModel();
-    Scanner input = new Scanner(System.in);
 
-    public void tambah(){
-        System.out.print("Kode Barang : ");
-        int kode = input.nextInt();
-        input.nextLine();
-        System.out.print("Masukkan Tipe Tas : ");
-        String nama = input.nextLine();
-        System.out.print("Masukkan Merk : ");
-        String merk = input.nextLine();
-        System.out.print("Masukkan Warna : ");
-        String warna = input.nextLine();
-        ptgs.insert(kode,nama, merk, warna);
-    }
-
-    public void update(){
-        System.out.print("id : ");
-        int kode = input.nextInt();
-        input.nextLine();
-        System.out.print("Masukan nama baru : ");
-        String nama = input.nextLine();
-        System.out.print("Masukkan telepon baru : ");
-        String merk = input.nextLine();
-        System.out.print("Masukkan password baru : ");
-        String warna = input.nextLine();
-        ptgs.Update(kode, nama, merk, warna);
-    }
-
-    public void Delete(){
-        int kodebarang;
-        System.out.print("Id yang akan dihapus ? : ");
-        kodebarang = input.nextInt();
-        ptgs.delete(kodebarang);
-    }
 
 
 }
--- src/Model/PetugasModel.java
+++ src/Model/PetugasModel.java
@@ -6,48 +6,5 @@ import java.util.*;
 
 public class PetugasModel {
 
-    ArrayList<Petugas>list= new ArrayList<>();
 
-//    public PetugasModel(){
-//        List = new ArrayList<>();
-//    }
-
-    public void insert(int id, String nama, String Telepon, String passsword){
-        list.add(new Petugas(id,nama,Telepon,passsword));
-    }
-
-    public int indexData(Petugas petugas){
-        int index = list.indexOf(petugas);
-        return index;
-    }
-
-    public Petugas FindByID(int kode){
-        for (Petugas petugas:list){
-            if(petugas.getId() == kode){
-                System.out.println("========================");
-                System.out.println("kode : "+petugas.getId());
-                System.out.println("Tipe : "+petugas.getNama());
-                System.out.println("Merk : "+petugas.getTelp());
-                System.out.println("Warna : "+petugas.getPassword());
-                return petugas;
-            }
-        }
-        return null;
-    }
-
-    public void Update(int KodeBarang, String nama, String merk, String warna){
-        Petugas caritas = FindByID(KodeBarang);
-
-        if(caritas != null){
-            int index = indexData(caritas);
-            list.get(index).setId(KodeBarang);;
-            list.get(index).setNama(nama);
-            list.get(index).setTelp(merk);
-            list.get(index).setPassword(warna);
-        }
-    }
-
-    public void delete(int index){
-        list.remove(index);
-    }
 }
